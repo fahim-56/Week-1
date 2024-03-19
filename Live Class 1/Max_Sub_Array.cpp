0174dbc9e0e4b1f7551fd1f5886953e88ec984d3
Md Fahim Chowdhury <163567991+fahim-56@users.noreply.github.com>
2024-03-19 12:30:47 +0600

    Max_Sub_Array.cpp

--- Live Class 1/Max_Sub_Array.cpp
+++ Live Class 1/Max_Sub_Array.cpp
@@ -1,42 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution
+long maximumSumSubarray(int k, vector<int> &Arr, int N)
 {
-public:
-    long maximumSumSubarray(int K, vector<int> &Arr, int N)
+    long sum = 0;
+    for (int i = 0; i < k; i++)
     {
+        sum += Arr[i];
     }
-};
+    long mx = 0;
+    mx = max(mx, sum);
+    for (int i = 0; i < N; i++)
+    {
+        if (k >= N)
+            break;
+        sum = sum - Arr[i] + Arr[k];
+        mx = max(mx, sum);
+        k++;
+    }
+    return mx;
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     int n, k;
     cin >> n >> k;
-    n = n + 1;
-    int arr[n];
-    for (int i = 1; i < n; i++)
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        int x;
+        cin >> x;
+        arr.push_back(x);
     }
 
-    int r = k;
-    long long int sum = 0;
-    for (int i = 1; i <= k; i++)
-    {
-        sum += arr[i];
-    }
-    // cout << sum << endl;
-    long long int mx = 0;
-    for (int i = 1; i < n; i++)
-    {
+    cout << maximumSumSubarray(k, arr, n);
 
-        sum = sum - arr[i] + arr[r ];
-        mx = max(mx, sum);
-        r++;
-        if (r >= n)
-            break;
-    }
-    cout << mx << endl;
     return 0;
 }

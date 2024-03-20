117f6a87f50121ac0c77864c687683a438694d3a
Md Fahim Chowdhury <163567991+fahim-56@users.noreply.github.com>
2024-03-20 23:54:47 +0600

    Add files via upload

---
+++ AC_Please.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    if (t <= 30)
+    {
+        cout << "NO" << endl;
+    }
+    else
+        cout << "YES" << endl;
+    return 0;
+}
